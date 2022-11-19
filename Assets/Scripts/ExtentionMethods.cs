94af5cc16d627dbc6f676642ac45c65d71bd2daf
Tijmen Matthys <tijmen.matthys@ugent.be>
2022-11-19 22:48:52 +0100

    ADD Z-Teleportation, still buggy

---
+++ Assets/Scripts/ExtentionMethods.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public static class ExtentionMethods
+{
+    public static Vector3 Abs(this Vector3 v)
+        => new Vector3(Mathf.Abs(v.x), Mathf.Abs(v.y), Mathf.Abs(v.z));
+}
