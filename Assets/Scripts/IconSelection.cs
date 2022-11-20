06a59b991e344e91e00e6ab9c53613c6361d0e29
EliasTristi <elias.triest@gmail.com>
2022-11-20 13:54:05 +0100

    figuring out the selection
    still working on this

--- Assets/Scripts/IconSelection.cs
+++ Assets/Scripts/IconSelection.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class IconSelection : MonoBehaviour
 {
     public float _lerpSpeed = 5;
 
+    public Vector3 _offset = Vector3.one * 10;
+
     void Start()
     {
-        
+
     }
 
     void Update()
@@ -17,10 +20,14 @@ public class IconSelection : MonoBehaviour
 
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit))
         {
+            Debug.DrawRay(transform.position, transform.forward * 100, Color.yellow);
             if (hit.collider != null)
             {
-                Debug.Log("collider detected");
-                Camera.main.transform.position = Vector3.Lerp(transform.position, hit.collider.transform.position, _lerpSpeed * Time.deltaTime);
+                Vector3 targetPosition = hit.transform.position + _offset;
+                Debug.Log(Vector3.Distance(transform.position, hit.transform.position));
+
+                //Debug.Log("collider detected");
+                transform.position = Vector3.Lerp(transform.position, targetPosition, _lerpSpeed * Time.deltaTime);
             }
         }
     }
